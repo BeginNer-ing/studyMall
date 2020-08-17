ee5e9e109d4c7ad04ae55719a77fd407b5dd5805
Yanis_CC <916380773@qq.com>
2020-08-17 19:00:21 +0800

    mall项目基本骨架和swagger2搭建

---
+++ microservice-01/src/main/java/studyMall/common/api/IErrorCode.java
@@ -0,0 +1,7 @@
+package studyMall.common.api;
+
+public interface IErrorCode {
+    long getCode();
+
+    String getMessage();
+}
---
+++ microservice-01/src/main/java/studyMall/service/PmsBrandService.java
@@ -0,0 +1,20 @@
+package studyMall.service;
+
+import org.springframework.stereotype.Service;
+import studyMall.mbg.model.PmsBrand;
+
+import java.util.List;
+
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
