6b1d19190332b72405392b40893c007e3ec85d5b
Yanis_CC <916380773@qq.com>
2020-08-26 16:58:34 +0800

    mall项目redis搭建完成

--- microservice-01/src/main/java/studyMall/service/impl/RedisServiceImpl.java
+++ microservice-01/src/main/java/studyMall/service/impl/RedisServiceImpl.java
@@ -2,38 +2,42 @@ package studyMall.service.impl;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
 import studyMall.service.RedisService;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Description:
  * @Auther: Yanis_CC
  * @CreateDate: 2020-08-21 10:29
  **/
+@Service
 public class RedisServiceImpl implements RedisService {
     @Qualifier
     private StringRedisTemplate stringRedisTemplate;
     @Override
     public void set(String key, String value) {
-
+        stringRedisTemplate.opsForValue().set(key, value);
     }
 
     @Override
     public String get(String key) {
-        return null;
+        return stringRedisTemplate.opsForValue().get(key);
     }
 
     @Override
     public boolean expire(String key, long expire) {
-        return false;
+        return stringRedisTemplate.expire(key,expire, TimeUnit.SECONDS);
     }
 
     @Override
     public void remove(String key) {
-
+        stringRedisTemplate.delete(key);
     }
 
     @Override
     public Long increment(String key, long delta) {
-        return null;
+        return stringRedisTemplate.opsForValue().increment(key,delta);
     }
 }
